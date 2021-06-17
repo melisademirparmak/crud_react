185a367ccda868a3684e30547e0173e00939a30d
melisademirparmak <melisademirparmaak@gmail.com>
2021-06-17 20:36:46 +0300

    App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,32 @@ function App() {
   return (
     <div className="App">
       <EmployeeList />
+      <div className="container-xl">
+        <div className="table-responsive">
+          <div className="table-wrapper">
+            <div className="table-title">
+              <div className="row">
+                <div className="col-sm-6">
+                  <h2>
+                    Manage <b>Employees</b>
+                  </h2>
+                </div>
+                <div className="col-sm-6">
+                  <a
+                    href="#addEmployeeModal"
+                    className="btn btn-success"
+                    data-toggle="modal"
+                  >
+                    <i className="material-icons">&#xE147;</i>
+                    <span>Add New Employee</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <EmployeeList />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
