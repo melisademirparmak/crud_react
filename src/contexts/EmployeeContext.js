fb2fbd5be176be984352920d12bdd881add8250f
melisademirparmak <melisademirparmaak@gmail.com>
2021-06-18 12:45:16 +0300

    uuid imported

--- src/contexts/EmployeeContext.js
+++ src/contexts/EmployeeContext.js
@@ -1,39 +1,40 @@
 import { createContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export const EmployeeContext = createContext();
 
 const EmployeeContextProvider = (props) => {
   const [employees] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       name: 'Thomas Hardy',
       email: 'thomashardy@mail.com',
       address: '89 Chiaroscuro Rd, Portland, USA',
       phone: '(171) 555-2222',
     },
     {
-      id: 2,
+      id: uuidv4(),
       name: 'Dominique Perrier',
       email: 'dominiqueperrier@mail.com',
       address: 'Obere Str. 57, Berlin, Germany',
       phone: '(313) 555-5735',
     },
     {
-      id: 3,
+      id: uuidv4(),
       name: 'Maria Anders',
       email: 'mariaanders@mail.com',
       address: '25, rue Lauriston, Paris, France',
       phone: '(503) 555-9931',
     },
     {
-      id: 4,
+      id: uuidv4(),
       name: 'Fran Wilson',
       email: 'franwilson@mail.com',
       address: 'C/ Araquil, 67, Madrid, Spain',
       phone: '(204) 619-5731',
     },
     {
-      id: 5,
+      id: uuidv4(),
       name: 'Martin Blank',
       email: 'martinblank@mail.com',
       address: 'Via Monte Bianco 34, Turin, Italy',
