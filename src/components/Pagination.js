cd9adaf718a383b31ea37616c8c6dfe1a0c5a9d5
melisademirparmak <melisademirparmaak@gmail.com>
2021-06-21 13:15:11 +0300

    Push numofPages

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,11 @@
 const Pagination = () => {
-    return()
-}
+  const pages = 5;
 
+  const numOfPages = [];
+  for (let i = 1; i <= pages; i++) {
+    numOfPages.push(i);
+  }
+  return <div>Pagination</div>;
+};
 
 export default Pagination;
