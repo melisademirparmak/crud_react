1863b1cec34cf7c46bd29a6e4707b8f924589a68
melisademirparmak <melisademirparmaak@gmail.com>
2021-06-23 16:55:58 +0300

    Tooltip

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,6 +1,6 @@
 import { EmployeeContext } from '../contexts/EmployeeContext';
 import { useContext, useState, useEffect } from 'react';
-import { Modal } from 'react-bootstrap';
+import { Modal, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import EditForm from './EditForm';
 
 const Employee = ({ employee }) => {
@@ -21,24 +21,20 @@ const Employee = ({ employee }) => {
       <td>{employee.address}</td>
       <td>{employee.phone}</td>
       <td className="row align-items-center">
-        <buttton
-          onClick={handleShow}
-          className="btn text-warning row"
-          data-toggle="modal"
-        >
-          <i className="material-icons" data-toggle="tooltip" title="Edit">
-            &#xE254;
-          </i>
-        </buttton>
-        <button
-          onClick={() => deleteEmployee(employee.id)}
-          className="btn text-danger"
-          data-toggle="modal"
-        >
-          <i className="material-icons" data-toggle="tooltip" title="Delete">
-            &#xE872;
-          </i>
-        </button>
+        <OverlayTrigger overlay={<Tooltip id={`tooltip-top`}>Edit</Tooltip>}>
+          <buttton onClick={handleShow} className="btn text-warning row">
+            <i className="material-icons">&#xE254;</i>
+          </buttton>
+        </OverlayTrigger>
+
+        <OverlayTrigger overlay={<Tooltip id={`tooltip-top`}>Delete</Tooltip>}>
+          <button
+            onClick={() => deleteEmployee(employee.id)}
+            className="btn text-danger"
+          >
+            <i className="material-icons">&#xE872;</i>
+          </button>
+        </OverlayTrigger>
       </td>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header className="modal-header" closeButton>
